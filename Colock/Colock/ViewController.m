e602fa1f3290a57ade69f609c8e561d21eb39f9d
susu <28025920@qq.com>
2016-04-24 20:13:02 +0800

    创建项目，添加素材

---
+++ Colock/Colock/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  Colock
+//
+//  Created by  江苏 on 16/4/24.
+//  Copyright © 2016年 jiangsu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property (strong, nonatomic) IBOutlet UIImageView *clock;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+
+@end
